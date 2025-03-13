453daa7a729663d4b3a8501320249c83f70ddbcb
Jiayi Xu <jixy2359@gmail.com>
2025-03-13 15:58:48 -0400

    ajouts dune sidebar

---
+++ js/sidebar.js
@@ -0,0 +1,18 @@
+class SidebarManager {
+    constructor() {
+        this.sidebar = document.querySelector('.sidebar');
+        this.toggleBtn = document.querySelector('.sidebar-toggle');
+        this.init();
+    }
+
+    init() {
+        this.toggleBtn.addEventListener('click', () => this.toggle());
+    }
+
+    toggle() {
+        this.sidebar.classList.toggle('collapsed');
+        
+        localStorage.setItem('sidebarCollapsed', this.sidebar.classList.contains('collapsed'));
+    }
+}
+const sidebarManager = new SidebarManager();
