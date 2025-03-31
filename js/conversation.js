0a89d484efc7d32534256dbb67acc93b1eea9cf2
izakl <izak.lussier@icloud.com>
2025-03-31 14:25:17 -0400

    Implémentation de la création d'une nouvelle conversation

--- js/conversation.js
+++ js/conversation.js
@@ -23,7 +23,7 @@ const getConvo = () => {
                     const listItem = $('<li></li>')
                     const button = $('<button></button>').addClass('chat-item')
             
-                    const chatName = $('<span></span>').addClass('chat-name').text(convo.name || "Unnamed Conversation")
+                    const chatName = $('<span></span>').addClass('chat-name').text(convo.chat_name || "Unnamed Conversation")
             
                     // Formater la date
                     const chatDate = $('<span></span>').addClass('chat-date').text(formatDate(convo.date))
@@ -39,6 +39,56 @@ const getConvo = () => {
     .catch(error => {
         console.error("Erreur lors de l'obtention des conversations :", error)
         alert("Une erreur est survenue, veuillez réessayer.")
+    })
+}
+
+const newConvo = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const id_invite = $('#id-invite').val();
+    const chat_name = $('#nom-convo').val();
+
+    if (!user || !user.id) {
+        alert("Utilisateur non connecté !")
+        return
+    }
+
+    if (!id_invite || !chat_name) {
+        alert("Veuillez remplir tous les champs !")
+        return
+    }
+
+    const dataLogin = {
+        chat_name: chat_name,
+        id_utilisateur: user.id,
+        id_invite: id_invite,
+    };
+
+    const newChatButton = $('#ajouter-Convo')
+    newChatButton.prop('disabled', true).text('Ajout en cours...')
+
+    fetch("http://localhost:8000/api/convo/new", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataLogin)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        alert("Conversation créée avec succès !")
+        location.reload()
+    })
+    .catch(error => {
+        console.error("Erreur lors de la création :", error);
+        alert("Une erreur est survenue, veuillez réessayer.");
+    })
+    .finally(() => {
+        newChatButton.prop('disabled', false).text('Ajouter');
     })
 }
 
